2e4bb9a16555fd4a2370b371c6c98c0c5ac1740e
hzw <405163706@qq.com>
2019-02-27 17:54:50 +0800

    g

---
+++ learn20-自带优化/src/index.js
@@ -0,0 +1,3 @@
+import { minus, sum } from './main.js'
+
+minus(5, 3)
---
+++ learn20-自带优化/src/main.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+// ReactDOM.render(<div>good</div>, document.querySelector('#root'))
+
+const sum = (a, b) => {
+  console.log(a+b, 'sum')
+}
+
+const minus = (a, b) => {
+  console.log(a-b, 'minus')
+}
+
+export  {
+  sum,
+  minus
+}
